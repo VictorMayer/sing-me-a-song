b430d45fff534b63b73ac7e68c71dc57c4dbdfce
victorMayer <victor.mayer@outlook.com>
2021-12-05 12:39:31 -0300

    post routes implemented. testing all routes still needed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 import cors from 'cors';
 import express from 'express';
+import * as songController from './controllers/songController.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get('/health', (req, res) => res.send('Bolezinha'));
+
+app.post('/recommendations', songController.newSong);
+app.post('/recommendations/:id/upvote', songController.upvote);
+app.post('/recommendations/:id/downvote', songController.downvote);
 
 export default app;
